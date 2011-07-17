673864c6c53f2b3ec27e40fba8f4da16bcad04e1
Alexander Schlaich <a.schlaich@gmx.de>
2011-07-17 20:56:36 +0200

    Using entity pointers for communicating iPBS elems

--- src/ipbs_P1.hh
+++ src/ipbs_P1.hh
@@ -80,10 +80,6 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
     std::cout << "Hello I'm rank " << colCom.rank() << " of " << colCom.size() <<
       " and I have " << gv.size(0) << " codim<0> leaf elements." << std::endl;
 
-  int elemCounter = 0;    // debugging variable - count elements on this processor
-  int boundCounter = 0;   // debugging variable - count boundary elements on this processor
-  int ipbsCount = 0;      // count IPBS boundary elements on this processor
-  
   // we store element indices in the order they are found on each processor,
   // so we do not need a mapper but can use an array of appropriate size
   // all other containers use that order and finally a global one with elements
@@ -95,11 +91,7 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
   
   // store ipbs_elements
   std::vector<int> ipbsIndices;
-  // provide a vector storing the positions, will be resized automatically
-  std::vector<Dune::FieldVector<double,dim> > boundaryElemPositions;
-  // provide a vector storing the normals, will be resized automatically
-  std::vector<Dune::FieldVector<double,dim> > boundaryElemNormals;
-  // provide a vector storing the type of the iterative boundary
+    // provide a vector storing the type of the iterative boundary
   std::vector<int> ipbsType;
 
   // typedef iterators
@@ -107,28 +99,22 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
           <Dune::Interior_Partition>::Iterator LeafIterator;
   typedef typename GV::IntersectionIterator IntersectionIterator;
   
+  std::vector<LeafIterator> ipbsElemPointers;
+
   // loop over elements on this processor
   for (LeafIterator it = gv.template begin<0,Dune::Interior_Partition>();
             	it!=gv.template end<0,Dune::Interior_Partition>(); ++it)
   {
-    elemCounter++;
     if(it->hasBoundaryIntersections() == true)
     {
       for (IntersectionIterator ii = gv.ibegin(*it); ii != gv.iend(*it); ++ii)
       {
         if(ii->boundary() == true)
         {
-          boundCounter++;
           if (boundaryIndexToEntity[ii->boundarySegmentIndex()] > 1) // check if IPBS boundary
           {
-            if (sysParams.get_verbose() == 5) // verbose output
-              std::cout << "Boundary Center " << ii->geometry().center() << " on rank "
-                << colCom.rank() << std::endl;
-            ipbsCount ++;
+            ipbsElemPointers.push_back(it);
             ipbsIndices.push_back(boundaryElemMapper.map(*it));
-            boundaryElemPositions.push_back(ii->geometry().center());
-            // remember that the normals point outwards, i.e. when using them we'll have to turn around
-            boundaryElemNormals.push_back(ii->centerUnitOuterNormal());
             ipbsType.push_back(boundaryIndexToEntity[ii->boundarySegmentIndex()]);
           }
         }
@@ -136,29 +122,6 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
     }
   }
 
-  // verbose output
-  if (sysParams.get_verbose() > 1)
-  {
-    std::cout << "counted " << elemCounter << " Elements on process " << colCom.rank()
-      << " and " << boundCounter << " Boundaries on process " << colCom.rank()
-      << " of which " << ipbsCount << " are of iterated type." << std::endl;
-    std::cout << "ipbsIndices has size: " << ipbsIndices.size() << std::endl;
-    std::cout << "Stored positions are: " << std::endl;
-    for(unsigned int i = 0; i < boundaryElemPositions.size(); ++i)
-    {
-      std::cout << boundaryElemPositions[i] << std::endl;
-    }
-    std::cout << "Stored normals are: " << std::endl;
-    for(unsigned int i = 0; i < boundaryElemNormals.size(); ++i)
-    {
-      std::cout << boundaryElemNormals[i] << std::endl;
-    }
-    std::cout << "Store Map looks like:" << std::endl;
-    for(unsigned int i = 0; i < ipbsIndices.size(); ++i)
-    {
-      std::cout << i << "\t" << ipbsIndices[i] << "\t" << std::endl;
-    }
-  }
 
   // ipbsIndices now contains the local indices of iterative boundary elements
   // and boundaryElemPositions the position vectors on each processor
@@ -171,13 +134,13 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
   std::vector<int> length_on_processor;
   if (colCom.rank() == 0)
   {
-    length_on_processor.push_back(boundaryElemPositions.size());
+    length_on_processor.push_back(ipbsElemPointers.size());
   }
   for (int i = 1; i < colCom.size(); i++) 
   {
     if (colCom.rank() == i)
     {
-      int temp = boundaryElemPositions.size();
+      int temp = ipbsElemPointers.size();
       MPI_Send(&temp,1,MPI_INT,0,colCom.rank(),MPI_COMM_WORLD);
     }
     else if (colCom.rank() == 0)
@@ -202,90 +165,37 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
   
   // now master process receives all positions from other processes
   // first every processor creates an array of its position vectors
-  double* my_positions = (double*) malloc(dim*boundaryElemPositions.size()*sizeof(double));
-  double* my_normals = (double*) malloc(dim*boundaryElemNormals.size()*sizeof(double));
   int* my_types = (int*) malloc (ipbsType.size()*sizeof(int));
-  for (unsigned int i = 0; i<boundaryElemPositions.size(); i++){
-    for (int j = 0; j<dim; j++)
-    {
-      my_positions[i*dim+j] = boundaryElemPositions.at(i).vec_access(j);
-      my_normals[i*dim+j] = boundaryElemNormals.at(i).vec_access(j);
-    }
-    my_types[i] = ipbsType[i];
+  for (unsigned int i = 0; i<countBoundElems; i++){
+        my_types[i] = ipbsType[i];
   }
   
-  if (sysParams.get_verbose() > 4)
-  {
-    std::cout << "On rank " << colCom.rank() << " positions are:" << std::endl;
-    for (unsigned int i = 0; i<boundaryElemPositions.size(); i++){
-      for (int j = 0; j<dim; j+=2)
-        std::cout << my_positions[i*dim+j] << my_positions[i*dim+j+1] << std::endl;
-    }
-    std::cout << "On rank " << colCom.rank() << " normals are:" << std::endl;
-    for (unsigned int i = 0; i<boundaryElemNormals.size(); i++){
-      for (int j = 0; j<dim; j+=2)
-        std::cout << my_normals[i*dim+j] << my_normals[i*dim+j+1] << std::endl;
-    }
-  }
-
   int indexcounter;   // Count how many positions we already got
-  double* all_positions = (double*) malloc(dim*countBoundElems*sizeof(double)); // allocate on all processors
-  double* all_normals = (double*) malloc(dim*countBoundElems*sizeof(double)); // allocate on all processors
   int* all_types = (int*) malloc(countBoundElems*sizeof(int));
   if( colCom.rank() !=0) // other nodes send their positions to master node
   {
-    MPI_Send(my_positions,dim*boundaryElemPositions.size(),MPI_DOUBLE,0,0,MPI_COMM_WORLD); // pos sent on slot 0
-    MPI_Send(my_normals,dim*boundaryElemPositions.size(),MPI_DOUBLE,0,1,MPI_COMM_WORLD); // normals sent on slot 1
-    MPI_Send(my_types,boundaryElemPositions.size(),MPI_INT,0,2,MPI_COMM_WORLD); // types sent on slot 2
+    MPI_Send(my_types,countBoundElems,MPI_INT,0,2,MPI_COMM_WORLD); // types sent on slot 2
   }
   if (colCom.rank() == 0)
   {
     // Write positions of master node
-    for (unsigned int i = 0; i<boundaryElemPositions.size(); i++){
-      for (int j = 0; j<dim; j++)
-      {
-        all_positions[i*dim+j] = boundaryElemPositions[i][j];
-        all_normals[i*dim+j] = boundaryElemNormals[i][j];
-      }
-      all_types[i] = ipbsType[i];
+    for (unsigned int i = 0; countBoundElems; i++){
+       all_types[i] = ipbsType[i];
     }
     // get the right offset
     indexcounter = dim*length_on_processor[0];
-    
-    if (sysParams.get_verbose() == 5)
-      std::cout << "After performing processor 0 indexcounter is: " << indexcounter << std::endl;
     
     // get positions from other nodes
     for (int i = 1; i < colCom.size(); i++) {
       if (sysParams.get_verbose() > 3)
         std::cout << "Length on processor " << i << " is " << length_on_processor[i] << std::endl;
-      MPI_Recv(&all_positions[indexcounter],dim*length_on_processor[i],MPI_DOUBLE,i,0,MPI_COMM_WORLD,&status);
-      MPI_Recv(&all_normals[indexcounter],dim*length_on_processor[i],MPI_DOUBLE,i,1,MPI_COMM_WORLD,&status);
-      MPI_Recv(&all_types[indexcounter/dim],length_on_processor[i],MPI_INT,i,2,MPI_COMM_WORLD,&status);
-      indexcounter += dim*length_on_processor[i];
+      MPI_Recv(&all_types[indexcounter],length_on_processor[i],MPI_INT,i,2,MPI_COMM_WORLD,&status);
+      indexcounter += length_on_processor[i];
     }
    }
 
   // deploy the iterative boundary positions on all processors
-  colCom.broadcast(all_positions,countBoundElems*dim,0); // communicate array
-  colCom.broadcast(all_normals,countBoundElems*dim,0); // communicate array
   colCom.broadcast(all_types,countBoundElems,0); // communicate array
-  if (sysParams.get_verbose() > 3)
-  {
-    std::cout << "on rank " << colCom.rank() << " full iterative boundary elements positions vectors:" << std::endl;
-    for (unsigned int i = 0; i<countBoundElems; i++){
-      for (int j = 0; j<dim; j+=2)
-        if (sysParams.get_verbose() > 3)
-          std::cout << all_positions[i*dim+j] << "\t" << all_positions[i*dim+j+1] << std::endl;
-    }
-    std::cout << "on rank " << colCom.rank() << " full iterative boundary elements normals vectors:" << std::endl;
-    for (unsigned int i = 0; i<countBoundElems; i++){
-      for (int j = 0; j<dim; j+=2)
-        if (sysParams.get_verbose() > 3)
-          std::cout << all_normals[i*dim+j] << "\t" << all_normals[i*dim+j+1] << std::endl;
-    }
-
-  }
 
   // ================================================================== 
   // Finally, we need a map so we can reverse lookup the positions of the ipbs_indices
@@ -336,8 +246,8 @@ void ipbs_P1(GridType* grid, const std::vector<int>& elementIndexToEntity,
     sysParams.reset_error();
 
     // call the function precomputing the boundary flux values
-    ipbs_boundary(gv,udgf, all_positions, all_normals, fluxContainer, countBoundElems, boundaryIndexToEntity,
-        indexLookupMap, boundaryElemMapper);
+    ipbs_boundary(gv,udgf, ipbsElemPointers, fluxContainer, countBoundElems,
+        boundaryIndexToEntity, indexLookupMap, boundaryElemMapper);
     // make sure each processor has finished calculations before proceeding
     MPI_Barrier(MPI_COMM_WORLD);
     if (sysParams.get_verbose() > 2)
