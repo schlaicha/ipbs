07330ac0edcede55878a6f6dd67d1d6c8f14fd50
Alexander Schlaich <phate@kallisto.(none)>
2011-07-19 00:22:11 +0200

    started implementing correct cylindrical symmetry

--- src/PBLocalOperator.hh
+++ src/PBLocalOperator.hh
@@ -94,23 +94,34 @@ public:
 
         RF f=0.;
       	// Parameters describing the PDE
-        switch (sysParams.get_salt())
-        {
-          case 0:
-            f = -1.0 * sysParams.get_lambda2i() * sinh(u);
-            break;
-          case 1:
-            f = -1.0 * sysParams.get_lambda2i() * exp(u);
-            break;
-        }
+        // switch (sysParams.get_salt())
+        // {
+        //   case 0:
+        //     f = -1.0 * sysParams.get_lambda2i() * sinh(u);
+        //     break;
+        //   case 1:
+        //     f = -1.0 * sysParams.get_lambda2i() * exp(u);
+        //     break;
+        // }
       	RF a = 0.; 
 
         // integrate grad u * grad phi_i + a*u*phi_i - f phi_i
         RF factor = it->weight()*eg.geometry().integrationElement(it->position());
 
+        // choose correct metric for integration
+        RF metric, integral;
+        switch ( sysParams.get_symmetry() )
+        {
+                case 1: metric = 1.0; integral=0.; break; // "2D_cylinder"
+                case 2: metric = 1.0 * globalpos[1]*2.0*sysParams.pi;
+                        integral = - 2.0*sysParams.pi*gradu[1]*factor;
+                        break;   // "2D_sphere"
+                case 3: metric = 1.0; integral=0.; break; // "3D"
+                default:    metric = 0.0; std::cerr << "Error: Could not detect metric" << std::endl;
+        }
        // Integration with added term for metric
         for (size_type i=0; i<lfsu.size(); i++)
-          r[i] += ( gradu*gradphi[i] + a*u*phi[i] - f*phi[i] )*factor;
+          r[i] += ( gradu*gradphi[i] + a*u*phi[i] - f*phi[i] )*factor*metric + integral*phi[i];
       }
   }
 
@@ -162,9 +173,19 @@ public:
 	
       	// integrate j
         RF factor = it->weight()*ig.geometry().integrationElement(it->position());
+        // choose correct metric for integration
+        RF metric;
+        switch ( sysParams.get_symmetry() )
+        {
+                case 1: metric = 1.0; break; // "2D_cylinder"
+                case 2: metric = 1.0; //*local[1]*2.0*sysParams.pi; break;   // "2D_sphere"
+                case 3: metric = 1.0; break; // "3D"
+                default:    metric = 0.0; std::cerr << "Error: Could not detect metric" << std::endl;
+        }
+       
         // Integration with added term for metric
         for (size_type i=0; i<lfsv_s.size(); i++)
-          r_s[i] += y*phi[i]*factor;
+          r_s[i] += y*phi[i]*factor*metric;
       }
   }
   
