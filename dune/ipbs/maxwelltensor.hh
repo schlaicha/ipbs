schlaicha/ipbs

commit 827f825e35d0b3f9f0911ae4833a792eed2ce6eb

small changes and developement

dune/ipbs/maxwelltensor.hh
@@ -58,7 +58,7 @@ Dune::FieldMatrix<Real, GFS::Traits::GridViewType::dimension,
   for (int i = 0; i < dim; i++)
   {
     //res[i][i] -= 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * .5 * gradphi.two_norm2();
-    res[i][i] -= 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * ( sysParams.get_lambda2i() * (std::cosh(value) - 1.0) + .5 * gradphi.two_norm2());
+    res[i][i] += 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * ( sysParams.get_lambda2i() * (std::cosh(value) - 1.0) - .5 * gradphi.two_norm2());
   }
   return res;
 }

dune/ipbs/PBLocalOperator.hh
@@ -168,6 +168,8 @@ public:
         // evaluate flux boundary condition
 	      typename J::Traits::RangeType y;
       	j.evaluate(ig, y);
+
+//        std::cout << global << " " << y << std::endl;
 	
       	// integrate j
         RF factor = it->weight()*ig.geometry().integrationElement(it->position());

dune/ipbs/bcextension.hh
@@ -44,7 +44,9 @@ public :
         return true;
       return false;
     }
-    else DUNE_THROW(Dune::NotImplemented,"Dirichlet interpolation for 3d still has to be done");
+  //  else DUNE_THROW(Dune::NotImplemented,"Dirichlet interpolation for 3d still has to be done");
+    else 
+        return false;
   }
 
 

dune/ipbs/ipbs_Pk.hh
@@ -195,7 +195,7 @@ void ipbs_Pk(GridType* grid, const PGMap& elementIndexToEntity,
   NEWTON newton(go,u,ls);
   newton.setLineSearchStrategy(newton.hackbuschReuskenAcceptBest);
   newton.setVerbosityLevel(sysParams.get_verbose());
-  newton.setMinLinearReduction(1e-6);
+  newton.setMinLinearReduction(1e-9);
   newton.setMaxIterations(100);
   newton.setLineSearchMaxIterations(50);
 

dune/ipbs/ipbsolver.hh
@@ -160,7 +160,7 @@ class Ipbsolver
 
           // select quadrature rule
           Dune::GeometryType gt = it->geometry().type();
-          unsigned int io = (int) ceil( 10*pow(ipbsVolumes[i], 1./(dim-1)) / sqrt( (ipbsPositions[i]-it->geometry().center())*(ipbsPositions[i]-it->geometry().center())));
+          unsigned int io = (int) ceil( sysParams.get_integration_maxintorder()*pow(ipbsVolumes[i], 1./(dim-1)) / sqrt( (ipbsPositions[i]-it->geometry().center())*(ipbsPositions[i]-it->geometry().center())));
           if (io>51)
               io=51;
           const Dune::QuadratureRule<DF,dim>& 

dune/ipbs/parser.cc
@@ -65,6 +65,7 @@ void parser(std::string config_file)
   
   sysParams.set_integration_l(configuration.get<double>("solver.l", 0.15*sysParams.get_lambda()));
   sysParams.set_integration_d(configuration.get<double>("solver.d", 0.075*sysParams.get_lambda()));
+  sysParams.set_integration_maxintorder(configuration.get<double>("solver.maxintorder", 10));
 
   // Output
   sysParams.set_outStep(configuration.get<int>("output.steps",0));

dune/ipbs/sysparams.cc
@@ -196,6 +196,14 @@ double SysParams::get_integration_d() {
     return integration_d;
 }
 
+void SysParams::set_integration_maxintorder(double value) {
+    integration_maxintorder = value;
+}
+
+double SysParams::get_integration_maxintorder() {
+    return integration_maxintorder;
+}
+
 void SysParams::set_pH(double pH_) {
     pH=pH_;
 }

dune/ipbs/sysparams.hh
@@ -34,6 +34,7 @@ class SysParams {
   double get_pH();
   double get_integration_d();
   double get_integration_l();
+  double get_integration_maxintorder();
 
   // Functions setting the private members
   void add_error(double);
@@ -59,6 +60,7 @@ class SysParams {
   void set_pH(double value);
   void set_integration_d(double value);
   void set_integration_l(double value);
+  void set_integration_maxintorder(double value);
 	
   private:
 	double lambda;
@@ -80,6 +82,7 @@ class SysParams {
   int verbose;
   double integration_d;
   double integration_l;
+  double integration_maxintorder;
   double refinementFraction;
   int refinementSteps;
   double pH;

commit a937c1637663a6b39529ac81fb70007f239ca80b

BUGFIX: wrong sign in stress tensor reverts changes introduced in 827f825e35d0b3f9f0911ae4833a792eed2ce6eb

dune/ipbs/maxwelltensor.hh
@@ -58,7 +58,7 @@ Dune::FieldMatrix<Real, GFS::Traits::GridViewType::dimension,
   for (int i = 0; i < dim; i++)
   {
     //res[i][i] -= 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * .5 * gradphi.two_norm2();
-    res[i][i] += 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * ( sysParams.get_lambda2i() * (std::cosh(value) - 1.0) - .5 * gradphi.two_norm2());
+    res[i][i] -= 1.0/(4.0 * sysParams.pi * sysParams.get_bjerrum()) * ( sysParams.get_lambda2i() * (std::cosh(value) - 1.0) + .5 * gradphi.two_norm2());
   }
   return res;
 }